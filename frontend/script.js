1d26f6a6f934e9a9a43eb3d021f0c9f74a7c8506
floresz95 <florianpiricsi@gmail.com>
2022-05-11 11:40:32 +0200

    navbar animation added

--- frontend/script.js
+++ frontend/script.js
@@ -1,26 +1,78 @@
-const menuButtonComponent = function(){
-    return`
-    <button id="menu-btn"></button>
+const beerNavComponent = function (inner){
+    return `
+        <nav id="navigation">${inner}</nav>
     `
 }
 
+const menuButtonComponent = function(){
+    return`
+        <button id="menu-btn"></button>
+    `
+}
+
+const beerSectionComponent = function(title, sub, text, id){
+    return`
+        <section id="${id}">
+            <h1 class="beerName">${title}</h1>
+            <h2 class="beerCompany">${sub}</h2>
+            <h3 class="beerType">${text}</h3>
+            </section>
+    `
+}
+
+const beerAnchorsComponent = function (title, id){
+    return`
+        <a href ="${id}">${title}</a>
+    `
+}
+
+
+const menuButtonClickedEvent = function(event){ 
+    event.currentTarget.closest("#root").classList.toggle("menu-opened");
+}
+/* const navBarClickedEvent = function(event){ 
+    event.currentTarget.closest("#root").classList.toggle("menu-opened");
+} */
+
+
 const loadEvent = function() {
     const rootElement = document.getElementById("root")
-    console.log(rootElement.parentElement);
+    /* console.log(beerSectionComponent);
+    console.log(beerSectionComponent());
 
-    rootElement.insertAdjacentHTML("beforeend", menuButtonComponent());
-
-    const menuButtonElement = document.getElementById("menu-btn");
-
-    menuButtonElement.addEventListener("click", function(event){
-        //event.currentTarget.classList.toggle("clicked")
-        /* console.log(rootElement);
-        console.log(event.currentTarget.parentElement);
-        console.log(event.currentTarget.closest("#root")); */
-        event.currentTarget.closest("#root").classList.toggle("menu-opened");
-    })
-    //console.log(globalVariable1);
+    (function (){
+        console.log("wefbwfb");
+    })() */
     
+    rootElement.insertAdjacentHTML("beforeend", menuButtonComponent());
+    
+    const menuButtonElement = document.getElementById("menu-btn");
+    
+    menuButtonElement.addEventListener("click", menuButtonClickedEvent)
+    
+    let beerSections = "";
+    
+    for (const beer of beers.cards) {
+        beerSections += beerSectionComponent(beer.title, beer.sub, beer.text);
+    }
+    rootElement.insertAdjacentHTML("beforeend", beerSections);
+    //console.log(beerSections);
+
+    let beerAnchors = "";
+
+
+    for (const beer of beers.cards) {
+        beerAnchors += beerAnchorsComponent(beer.title)
+    }
+
+    rootElement.insertAdjacentHTML("beforeend", beerNavComponent(beerAnchors))
+
+    const navBarElement = document.getElementById("menu-btn");
+    
+    navBarElement.addEventListener("click", menuButtonClickedEvent)
 }
+
 window.addEventListener("load", loadEvent);
 
+/* // function után ha rakunk zárójelet akkor a return utáni rész jelenik meg, ha nem akkor maga a függvény | function() vagy function console.log(beerSectionComponent);
+    console.log(beerSectionComponent()); */
